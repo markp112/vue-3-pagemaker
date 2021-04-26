131c2dd2b38dd2a41718d687a661d9194530b8ee
mark phillips <mark.phillips1965@googlemail.com>
2021-04-26 18:34:53 +0100

    installed storybook

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import './index.css';
+// import { firestorePlugin } from "vuefire";
+import Firebase from "firebase/app";
+import "firebase/firestore";
+
+
+// export const firebaseApp = Firebase.initializeApp(secrets.google);
 
 createApp(App).use(store).use(router).mount("#app");
